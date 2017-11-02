41037808af1b64400d2df1c89b5d812400d6c2a3
meisj <254894243@qq.com>
2017-11-02 15:51:55 +0800

    555

--- src/com/imooc/concurrent/base/Test2.java
+++ src/com/imooc/concurrent/base/Test2.java
@@ -4,7 +4,7 @@ public class Test2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("6669999998888");
+		System.out.print("66699999988885555");
 
 	}
 
